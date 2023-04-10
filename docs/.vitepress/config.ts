fe91c8ed2fe03452528530bd7a8c4ea894bc61aa
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2023-04-10 11:38:14 -0500

    chore: added vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitepress';
+import { version } from '../../package.json';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: `Belt Builder v. ${version}`,
+  description: 'A VitePress Site',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
+  }
+});
